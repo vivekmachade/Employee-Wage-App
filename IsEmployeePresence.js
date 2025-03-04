0743275d255549b62b0740c79a20b38df0e83c52
vivekmachade <101501915+vivekmachade@users.noreply.github.com>
2025-03-04 12:39:07 +0530

    Delete IsEmployeePresence.js

--- IsEmployeePresence.js
+++
@@ -1,9 +0,0 @@
-//UC 1 
-const Is_Absent = 0 ;
-let empCheck = Math.floor(Math.random()*10 )%2;
-if(empCheck == Is_Absent){
-	console.log("Employee is Absent");
-	return;
-}else{
-	console.log("Employee is Present");
-}
