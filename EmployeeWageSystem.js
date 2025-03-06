1bd40711215d471b5a65b7087ec58faf35927e97
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-03-06 15:16:29 +0530

    [Vivek Machade] Add . calculate Employees wages using while loop

--- EmployeeWageSystem.js
+++ EmployeeWageSystem.js
@@ -14,19 +14,25 @@ function getWorkingHours(empCheck) {
     return empWage;
 }
 
-const TotalWorkingDays = 20;
-let empHrs = 0;
-
 const isPartTime = 1;
 const isFullTime = 2;
 const PartTimeHours = 4;
 const FullTimeHours = 8;
 const WagesPerHour = 20;
 
-for(let day = 1; day <= TotalWorkingDays; day++) {
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
-}
+const maxHours = 100;
+const workingDays = 10;
 
-let empWage = empHrs * WagesPerHour;
-console.log("Total Hrs: " + empHrs + " Employee Wage: " + empWage);d
+let totalEmpHrs = 0, totalWorkingDays = 0;
+
+while (totalEmpHrs <= maxHours && totalWorkingDays < workingDays) {
+
+    totalWorkingDays++;
+
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+} 
+
+let empWage = totalEmpHrs * WagesPerHour;
+
+console.log("Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Employee Wage: " + empWage);
